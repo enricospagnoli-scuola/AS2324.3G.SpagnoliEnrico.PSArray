00e30e8399ffc4a35e2a5fe0c2383018a5c46cbb
Enrico Spagnoli <espagnoli07@gmail.com>
2024-02-29 08:46:21 +0100

    Feat: funzione "CaricaVettori"

--- Program.cs
+++ Program.cs
@@ -8,14 +8,24 @@ class Program
         int dim = Convert.ToInt32(Console.ReadLine());
         double[] voti = new double[dim];
         int[] pesi = new int[dim];
-        int nVoti = 0;
+        int nVoti = dim;
         StampaVotiPesi(voti, pesi, nVoti);
     }
     static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
     {
+        CaricaVettori(ref voti, ref pesi, nVoti);
         for (int i = 0; i < pesi.Length; i++)
         {
             Console.WriteLine($"Voto: {voti[i]} Peso: {pesi[i]}");
+        }
+    }
+    static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+    {
+        Random random = new Random();
+        for (int i = 0; i < nVoti; i++)
+        {
+            voti[i] = random.Next(1, 11);
+            pesi[i] = random.Next(0, 101);
         }
     }
 }
